13aad5af8212d147d39cd71434b110d50d36ae10
Gavin Everett <gavin.everett48@hotmail.com>
2021-04-29 15:27:45 +0100

    sucessfully retrieving info from coingecko

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import './App.css';
+import axios from 'axios';
+import { useEffect } from 'react';
 
 function App() {
+
+//run function on load.
+
+useEffect(() => {
+  axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false').
+  then(response => {
+    console.log(response.data);
+  }).catch(error => console.log(error));
+}, []);
+
+
+
+
+
   return (
     <div className="App">
  
