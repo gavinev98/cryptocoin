49055b3bddda13bf43af85ed6791e1e6b058a610
Gavin Everett <gavin.everett48@hotmail.com>
2021-05-08 11:32:48 +0100

    using chart js 2, chart now rendering. refactor

--- src/components/CoinStuff/CoinSummary/CoinOverviewCharts.js
+++ src/components/CoinStuff/CoinSummary/CoinOverviewCharts.js
@@ -1,56 +1,39 @@
-import React from 'react';
-import {Chart, registerables } from 'chart.js';
-import {useEffect} from 'react';
-import classes from "./LineGraph.module.css";
+import React from "react";
+
+import { Line } from "react-chartjs-2";
 
 const CoinOverviewCharts = () => {
 
-    Chart.register(...registerables);
 
-    useEffect(() => {
-        var ctx = document.getElementById('myChart').getContext('2d');
-        var myChart = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                datasets: [{
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                }
-            }
-        });
-    });
-        
+    const data = {
+        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+        datasets: [
+          {
+            label: "First dataset",
+            data: [33, 53, 85, 41, 44, 65],
+            fill: true,
+            backgroundColor: "rgba(75,192,192,0.2)",
+            borderColor: "rgba(75,192,192,1)"
+          },
+          {
+            label: "Second dataset",
+            data: [33, 25, 35, 51, 54, 76],
+            fill: false,
+            borderColor: "#742774"
+          }
+        ]
+      };
+      
 
-    return (
-        <div>
-            <canvas id="myChart" width="400" height="400"></canvas>  
-        </div>
-    );
-};
+
+
+
+
+  return (
+    <div>
+        <Line data={data} />
+    </div>
+  )
+}
 
 export default CoinOverviewCharts;
