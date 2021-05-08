a3520f2a11d6145e33c1e1b0c36a79de3bdf0519
Gavin Everett <gavin.everett48@hotmail.com>
2021-05-08 12:43:08 +0100

    added lowest price in past 24 hours.

--- src/components/CoinStuff/CoinSummary/ListItem.js
+++ src/components/CoinStuff/CoinSummary/ListItem.js
@@ -5,6 +5,8 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
+import TrendingUpIcon from '@material-ui/icons/TrendingUp';
+import TrendingDownIcon from '@material-ui/icons/TrendingDown';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -22,18 +24,18 @@ export default function FolderList({selectedCoin}) {
       <ListItem>
         <ListItemAvatar>
           <Avatar>
-          
+          {selectedCoin.price_change_24h > 0 ? <TrendingUpIcon style={{ color: 'green' }}/> : <TrendingDownIcon color="action" />}
           </Avatar>
         </ListItemAvatar>
-        <ListItemText primary={`€`+ selectedCoin.price_change_24h} secondary="Price change in the last 24 hours" />
+        <ListItemText primary={`€`+ selectedCoin.price_change_24h} secondary="Price change in the last 24 hours." />
       </ListItem>
       <ListItem>
         <ListItemAvatar>
           <Avatar>
-           
+           <TrendingDownIcon color="primary" />
           </Avatar>
         </ListItemAvatar>
-        <ListItemText primary="Work" secondary="Jan 7, 2014" />
+        <ListItemText primary={`€`+ selectedCoin.low_24h} secondary="Lowest price in the past 24 hours." />
       </ListItem>
       <ListItem>
         <ListItemAvatar>
